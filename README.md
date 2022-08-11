f1d8e9e22e4a6d84be665524eac8e6289a6d1eec
Tiranoe <jungwoo9543@gmail.com>
2022-08-11 12:21:26 -0400

    move repo location

---
+++ README.md
@@ -0,0 +1,45 @@
+
+The game that I will be creating is called TAMAGOTCHI
+
+<Game's Title> How to raise a baby tutorial
+
+<Screentshots>
+![alt text](https://m.media-amazon.com/images/I/61odvUyLn8L._AC_SX679_.jpg)
+
+![alt text](https://twitter.com/lord_arse/status/1369422271115362305)
+
+<Technologies Used> I am using HTML, CSS, Javascript.
+
+<Getting Started>
+    Link to the game: DNE (Does not exist yet) - 08/07/22
+
+    Instructions in how to play the game:
+        One day, You found a random EGG in the middle of the road!
+        There is no one else around you, so you pick up the egg and take it back to your home and begin to take care of the egg.
+
+        The goal of this game is to continue to feed your "Pet" as time goes on until it becomes an adult being. Through the stages, you will need to continue to take care of your pet by:
+            - Feeding Food
+            - Sleeping
+            - Cleaning up after they poop
+            - Exercising
+            - Playing games
+            - Abandoning your Tamagotchi (You just want to watch the world burn huh)
+
+
+<Next Steps>
+    - Outline how the game is going to go
+        Use the Figma website to draw a map of map guide
+    
+    - Searching for images to use to display for each of the user's decision.
+
+    ***Figure out the time variable on how long user needs to play to complete the game, and also amount of time tamagotchi has before dying without attention.
+
+    Menu option(?)
+        - how the user can access the options of what to do with Tamagotchi
+
+
+Sources:
+Dropdown list - https://www.w3schools.com/howto/howto_css_dropdown.asp
+
+Health bar - https://stackoverflow.com/questions/20277052/how-to-make-a-health-bar
+
---
+++ main.js
@@ -0,0 +1,191 @@
+// Introduce all variables (Countdown timer, Health Factor, health limits, Game speed)
+//Game Max Limits
+//Testing this to see if I can connect the value from HTML file to update on javascript
+let hunger_Max = document.querySelector('.hungerBar').value;
+//console.log(hunger_Max) => shows as 50
+
+let boredom_Max = 50;
+let tired_Max = 50;
+let cleanness_Max = 50;
+let health_Max = hunger_Max + boredom_Max + tired_Max + cleanness_Max
+//console.log(health_Max) => shows as 200
+        //Question: which way to list the variable is better?
+
+// Name Constraints
+let playerName = document.getElementById('playerName');
+let tamaName = document.querySelector('#tamagotchiName');
+
+// Button Constraints
+const feedBtn = document.querySelector('#feedAction');
+const exerciseBtn = document.querySelector('#exerciseAction');
+const playBtn = document.querySelector('#playAction');
+const cleanBtn = document.querySelector('#cleanAction');
+const sleepBtn = document.querySelector('#sleepAction');
+const abandonBtn = document.querySelector('#abandonAction');
+
+//Level Contraints for the progress Bar
+const healthLevel = document.querySelector('.healthBar').value;
+const hungerLevel = document.querySelector('.hungerBar').value;
+const boredomLevel = document.querySelector('.boredomBar').value;
+const tiredLevel = document.querySelector('.sleepBar').value;
+const cleannessLevel = document.querySelector('.cleanBar').value;
+
+// Possibly use the Object Oriented Programming to keep track of Tamagotchi's condition?
+    // Tamagotchi (your pet) will be starting as an "egg" status
+
+class Tamagotchi {
+    constructor (name, health, hunger, play, sleep, clean) {
+    this.name = document.querySelector('#tamagotchiName');
+    this.health = health_Max;
+    this.hunger = hunger_Max;
+    this.play = boredom_Max;
+    this.sleep = tired_Max;
+    this.clean = cleanness_Max;
+    }
+
+// implement all updating values and adding it to sum.
+    //function to lose each stats constantly
+loseHunger(){
+    setInterval(() => 
+    {this.hunger -= 2;
+    this.health -= 2;
+    document.querySelector('.hungerBar').value = this.hunger}, 1000);
+}
+loseBoredom(){
+    setInterval(() => 
+    {this.play -= 2;
+    this.health -= 2;
+    document.querySelector('.boredomBar').value = this.play}, 1000);
+}
+loseSleep(){
+    setInterval(() => 
+    {this.sleep -= 2;
+    this.health -= 2;
+    document.querySelector('.sleepBar').value = this.sleep}, 1000);
+}
+loseCleanness(){
+    setInterval(() => 
+    {this.clean -= 2;
+    this.health -= 2;
+    document.querySelector('.cleanBar').value = this.clean}, 1000);
+}
+showStats(){
+    setInterval(() =>
+    console.log(this.health), 1000);
+}
+loseHealth(){
+    setInterval(() =>
+    {document.querySelector('.healthBar').value = this.health}, 1000);
+}
+
+//death function using IF statement
+death(){
+    if (this.health === 0 || this.sleep === 0 || this.hunger === 0 || this.play === 0){
+        alert('Uh-Oh! Your tamagotchi is dead!');
+    }
+    else return false;
+}
+
+//these functions are for all Tamagotchi's possible actions through player
+feed(){
+    this.hunger += 5;
+    this.health += 5;
+}
+exercise(){
+    this.hunger -= 1;
+    this.cleanness -= 1;
+    this.health += 2;
+}
+cleanUp(){
+    this.clean += 5;
+    this.health += 5;
+}
+sleeping(){
+    this.hunger -= 3;
+    this.sleep += 5;
+    this.health += 2;
+}
+playing(){
+    this.play += 10;
+    this.health += 10;
+}
+}
+
+let tmName = document.querySelector('.tamagotchiName')
+let tmgch = new Tamagotchi(tmName)
+
+//Invoking functions to continously lose health
+// Starts down the timer countdown for each health factors
+// Most likely want to display all Health factors for user to continue to keep track
+tmgch.loseHunger();
+tmgch.loseBoredom();
+tmgch.loseCleanness();
+tmgch.loseSleep();
+tmgch.loseHealth();
+tmgch.showStats();
+if(tmgch.death()){
+    alert('Uh-oh, your Tamagotchi is DEAD');
+};
+
+console.log(tmgch)
+
+// Menu bar to access all options (Player options)
+// Feed
+feedBtn.addEventListener('click', function() {
+    tmgch.feed();
+});
+// Exercise
+exerciseBtn.addEventListener('click', function() {
+    tmgch.exercise();
+});
+// Clean Up
+cleanBtn.addEventListener('click', function() {
+    tmgch.cleanUp();
+});
+// Sleep
+sleepBtn.addEventListener('click', function() {
+    tmgch.sleeping();
+})
+// Play Game
+playBtn.addEventListener('click', function() {
+    tmgch.playing();
+})
+
+// Abandon your Tamagotchi
+    // This option will restart the game
+    // And maybe I can add something extra to make the difficulty harder to shame the user
+
+
+//Death IF/ELSE statement
+
+
+//Need to make functions for each page that user will see per option
+
+
+// Game winning condition
+// Timer Countdown for Tamagotchi if it manages to become adult
+
+
+// User gets the ability to add their name to Top 10 list according to how well Tamagotchi has grown
+
+
+// Add secret achievements when user raises tamagotchi with 100 percent fill with all health factors
+
+
+// Give User choice to restart the game
+
+
+//running functions
+
+
+
+/*
+HTML - 
+<a href="#" id="action-sleep">Sleep</a>
+
+
+JAVASCRIPT - 
+const feedBtn = document.querySelector("#action-feed");
+
+
+*/
